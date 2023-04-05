72fe4fc12b15590773ba0ba864cdf8776b763e70
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-05 15:26:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,15 @@ def delete_movie(movies, selected_movie_info):
     st.success(f"{selected_movie_info['title']} has been deleted from the database.")
     return movies
 
+
 def main():
+    st.set_page_config(page_title="Movie Database", page_icon=":movie_camera:")
+
     st.title("Movie Database")
-    st.header("Select a movie from the dropdown list to view its details, rate it, or delete it.")
-    
+
+    menu = ["Home", "Add New Movie", "View Movie Info", "Delete Movie"]
+    choice = st.sidebar.selectbox("Select an option", menu)
+
     # List of example movies
     movies = [
         {"title": "The Shawshank Redemption", "released_year": 1994, "kind": "Drama", "nationality": "USA", "average_ranking": 4.7, "comments": ["Great movie!", "One of my all-time favorites."]},
@@ -50,20 +55,29 @@ def main():
         {"title": "The Lord of the Rings: The Fellowship of the Ring", "released_year": 2001, "kind": "Adventure", "nationality": "USA", "average_ranking": 4.5, "comments": ["Epic story!", "The special effects were amazing."]},
         {"title": "Forrest Gump", "released_year": 1994, "kind": "Drama", "nationality": "USA", "average_ranking": 4.4, "comments": ["Tom Hanks was perfect for this role.", "Heartwarming story."]}
         ]
-    # Create a list of movie titles for the dropdown menu
-    movie_titles = [movie["title"] for movie in movies]
+    
+    if choice == "Home":
+        st.write("Welcome to the Movie Database! Use the menu on the left to navigate.")
 
-    # Create a dropdown menu to select a movie
-    selected_movie_title = st.selectbox("Select a movie", movie_titles)
+    elif choice == "Add New Movie":
+        movies = add_new_movie(movies)
 
-    # Find the selected movie in the list of movies
-    selected_movie_info = None
-    if selected_movie_title:
-        selected_movie_info = next((movie for movie in movies if movie["title"] == selected_movie_title), None)
-        if selected_movie_info:
-            display_movie_info(selected_movie_info)
-        else:
-            st.error("Error: Could not find movie in database.")
+    elif choice == "View Movie Info":
+        selected_movie_title = st.selectbox("Select a movie", [movie["title"] for movie in movies])
+        selected_movie_info = [movie for movie in movies if movie["title"] == selected_movie_title][0]
+        display_movie_info(selected_movie_info)
+        add_comment(selected_movie_info)
+        display_comments(selected_movie_info)
+        rate_movie(selected_movie_info)
+
+    elif choice == "Delete Movie":
+        selected_movie_title = st.selectbox("Select a movie", [movie["title"] for movie in movies])
+        selected_movie_info = [movie for movie in movies if movie["title"] == selected_movie_title][0]
+        movies = delete_movie(movies, selected_movie_info)
+
+    st.sidebar.write("Movie List")
+    for movie in movies:
+        st.sidebar.write(movie["title"])            
 main()
 
     
