6ca64f4b06a36d7322543a8e6483ec3bbda9c76a
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-05 11:21:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from neo4j import GraphDatabase, basic_auth
+# from neo4j import GraphDatabase, basic_auth
 
 # Connect to Neo4j database
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
+# driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "password"))
 
 # Define Neo4j queries
-GET_ALL_MOVIES = "MATCH (m:Movie) RETURN m ORDER BY m.title ASC"
-GET_MOVIE_INFO = "MATCH (m:Movie {title: $title}) RETURN m.title, m.released_year, m.kind, m.nationality, m.average_ranking"
-ADD_MOVIE = "CREATE (m:Movie {title: $title, released_year: $released_year, kind: $kind, nationality: $nationality})"
-RATE_MOVIE = "MATCH (m:Movie {title: $title}) SET m.average_ranking = ($average_ranking + m.average_ranking) / 2"
+#GET_ALL_MOVIES = "MATCH (m:Movie) RETURN m ORDER BY m.title ASC"
+#GET_MOVIE_INFO = "MATCH (m:Movie {title: $title}) RETURN m.title, m.released_year, m.kind, m.nationality, m.average_ranking"
+#ADD_MOVIE = "CREATE (m:Movie {title: $title, released_year: $released_year, kind: $kind, nationality: $nationality})"
+#RATE_MOVIE = "MATCH (m:Movie {title: $title}) SET m.average_ranking = ($average_ranking + m.average_ranking) / 2"
 
 # Streamlit app layout
 st.title("Movie Database")
@@ -20,12 +20,12 @@ kind = st.sidebar.text_input("Kind")
 nationality = st.sidebar.text_input("Nationality")
 if st.sidebar.button("Add movie"):
     with driver.session() as session:
-        session.run(ADD_MOVIE, title=title, released_year=released_year, kind=kind, nationality=nationality)
+    #    session.run(ADD_MOVIE, title=title, released_year=released_year, kind=kind, nationality=nationality)
         st.sidebar.success(f"{title} has been added to the database.")
 
 with driver.session() as session:
     # Get all movies from database and add them to the dropdown list
-    movies = session.run(GET_ALL_MOVIES)
+   # movies = session.run(GET_ALL_MOVIES)
     movie_list = [movie["m"]["title"] for movie in movies]
     selected_movie = st.selectbox("Select a movie", movie_list)
 
@@ -40,11 +40,11 @@ with driver.session() as session:
     # Add a "Rate" button to rate the selected movie
     if st.button("Rate"):
         rating = st.number_input("Enter your rating (0-5)", min_value=0, max_value=5)
-        session.run(RATE_MOVIE, title=selected_movie, average_ranking=rating)
+        #session.run(RATE_MOVIE, title=selected_movie, average_ranking=rating)
         st.success(f"You rated {selected_movie} {rating} stars.")
 
     # Add a "Delete" button to delete the selected movie
     if st.button("Delete"):
-        session.run(f"MATCH (m:Movie {{title: '{selected_movie}'}}) DETACH DELETE m")
+       # session.run(f"MATCH (m:Movie {{title: '{selected_movie}'}}) DETACH DELETE m")
         st.success(f"{selected_movie} has been deleted from the database.")
 
