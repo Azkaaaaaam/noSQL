afbb8e86e735eaf0cdaadc546e51484999f4eb48
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-06 11:57:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import sqlite3
 #mongodb://_:<API-KEY>@global.aws.realm.mongodb.com:27020/?authMechanism=PLAIN&authSource=%24external&ssl=true&appName=data-mnfje:Cluster0:api-key
 
 
-client = pymongo.MongoClient("mongodb+srv://kamounazzap:Hello123.@cluster0.9gb1qb6.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://streamlit:streamlit@cluster0.9gb1qb6.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 movies_collection = db["moviesds.moviesds"]
 
