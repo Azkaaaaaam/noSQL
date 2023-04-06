f03421e18e7a07f96a711b91658309cc3e233dba
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-06 11:53:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import sqlite3
 
 client = pymongo.MongoClient("mongodb+srv://kamounazzap:Hello123.@cluster0.9gb1qb6.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
-movies_collection = db["moviesds"]
+movies_collection = db["moviesds.moviesds"]
 
 
 def display_movie_info(selected_movie_info):
