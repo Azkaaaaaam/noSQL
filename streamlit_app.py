274102d09dbe3e0ec1f14fe04b304bd9c09195f4
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-06 14:17:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,23 +123,23 @@ def main():
 
         # Column 2: Rate Movie
             with col2:
-            st.header("Rate Movie")
-            movie_titles = [movie["title"] for movie in movies_collection.find()]
-            selected_movie_title = st.selectbox("Select a movie", movie_titles)
-            selected_movie_info = movies_collection.find_one({"title": selected_movie_title})
-            st.write(f"Title: {selected_movie_info['title']}")
-            st.write(f"Released year: {selected_movie_info['year']}")
-            st.write(f"genre: {selected_movie_info['genre']}")
-            st.write(f"Nationality: {selected_movie_info['nationality']}")
-            st.write(f"Average rating: {selected_movie_info['average_rating']}")
-            display_comments(selected_movie_info)
-            st.write("\n")
-            rating = st.slider("Rate the movie", min_value=1, max_value=10, step=1)
-            submit_rating = st.button("Submit Rating")
-            if submit_rating:
-                old_rating, new_rating = update_rating(selected_movie_info, rating)
-                st.write(f"Old average rating: {old_rating}")
-                st.write(f"New average rating: {new_rating}")
+                st.header("Rate Movie")
+                movie_titles = [movie["title"] for movie in movies_collection.find()]
+                selected_movie_title = st.selectbox("Select a movie", movie_titles)
+                selected_movie_info = movies_collection.find_one({"title": selected_movie_title})
+                st.write(f"Title: {selected_movie_info['title']}")
+                st.write(f"Released year: {selected_movie_info['year']}")
+                st.write(f"genre: {selected_movie_info['genre']}")
+                st.write(f"Nationality: {selected_movie_info['nationality']}")
+                st.write(f"Average rating: {selected_movie_info['average_rating']}")
+                display_comments(selected_movie_info)
+                st.write("\n")
+                rating = st.slider("Rate the movie", min_value=1, max_value=10, step=1)
+                submit_rating = st.button("Submit Rating")
+                if submit_rating:
+                    old_rating, new_rating = update_rating(selected_movie_info, rating)
+                    st.write(f"Old average rating: {old_rating}")
+                    st.write(f"New average rating: {new_rating}")
 
     # View movie details
     st.sidebar.write("\n")
